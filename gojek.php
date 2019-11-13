fe93d7ccc77f88e2bc43d3951535b55de044ab8f
coknet <57169925+coknet@users.noreply.github.com>
2019-11-13 11:21:46 +0700

    Delete gojek.php

--- gojek.php
+++
@@ -1,8 +0,0 @@
-<?php date_default_timezone_set('Asia/Jakarta');
-include "function.php";
-echo color("red","[][][][][][][][][][][][][][][][][][][][][]");
-echo color("red","\n[]  Auto Create Gojek X Redeem Voucher  []\n");
-echo color("red","[]  Creator : Sobat Santuy                []\n");
-echo "[]  Version : 6.9                     []\n";
-echo "[]  Time    : ".date('[d-m-Y] [H:i:s]')."   []\n";
-echo "[][][][][][][][][][][][][][][][][][][][][]\n\n";
